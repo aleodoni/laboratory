82d45f9ba8d7241a9bf48b07b26b1148ef363121
aleodoni <aleodoni@gmail.com>
2021-08-13 11:05:34 -0300

    feat: Add request example download images as streams

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,1 +1,24 @@
 import request from 'request';
+import fs from 'fs';
+
+const urls = [
+  'https://www.olimpiadatododia.com.br/wp-content/uploads/2021/06/Filipe-Toledo-etapa-do-surf-ranch-do-mundial-de-surfe.jpeg',
+  'https://hardcore.com.br/wp-content/uploads/sites/2/2020/04/italo.jpeg',
+];
+
+async function download(): Promise<void> {
+  const { log } = console;
+
+  Object.entries(urls).forEach(async ([id, url]) => {
+    request(url)
+      .on('complete', () => {
+        log('Completed');
+      })
+      .on('error', err => {
+        log(`Error: ${err}`);
+      })
+      .pipe(fs.createWriteStream(`assets/downloaded-${id}.jpeg`));
+  });
+}
+
+download();
